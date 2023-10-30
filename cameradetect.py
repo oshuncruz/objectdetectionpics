f0203f0b92c9a654fda9ebfa0f317ae91ef0a977
oshuncruz <37771084+oshuncruz@users.noreply.github.com>
2023-10-30 02:07:57 -0500

    Add files via upload
    Camera detection update using the sony remote software

--- cameradetect.py
+++ cameradetect.py
@@ -1,89 +1,89 @@
-import torch
-import cv2
 import subprocess
+import cv2
+import torch
 import time
+import pyautogui
+import pydirectinput
 
-last_capture_time = time.time()
+# Set the path to the Remote Camera Control software executable
+REMOTE_CONTROL_EXE = 'C:/Program Files/Sony/Imaging Edge/Remote.exe'
 
-capture_interval = 3
-# Load the YOLOv5 PyTorch model
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='C:/Users/oshun/OneDrive/Desktop/T-shirt-Object-Detection-master/yolov5/best2.pt')
+# Set the path where you want to save the captured images
+IMAGE_SAVE_PATH = 'C:/captured_images/'
 
+# Load the YOLOv5 model
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='C:/Users/Oshun/Desktop/camera/best.pt')
 
-def run_gphoto2():
-    global last_capture_time
-    
-    # Check if the minimum time between captures has passed
-    if time.time() - last_capture_time >= capture_interval:
-        # Define the command to run in the mingw64 terminal
-        cmd = 'C:/msys64/mingw64.exe gphoto2 --capture-image'
-        
-        # Open a new process with the command
-        process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        
-        # Read the output and error messages from the process
-        output, error = process.communicate()
-        
-        # Print the output and error messages
-        print(output.decode('utf-8'))
-        print(error.decode('utf-8'))
-        
-        # Update the last_capture_time variable to the current time
-        last_capture_time = time.time()
+# Set the confidence threshold for object detection
+confidence_threshold = 0.35
 
+# Class IDs to filter out
+skirt_id = 8
+short_sleeved_dress_id = 9
+long_sleeved_dress_id = 10
+vest_dress_id = 11
+sling_dress_id = 12
 
-# Define the video capture device (in this case, the default webcam)
-cap = cv2.VideoCapture(0)
+# Connect to the camera
+subprocess.Popen([REMOTE_CONTROL_EXE, '-c'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-confidence_threshold = 0.5
+# Wait for the camera to connect
+time.sleep(5)
+
+# Open the remote camera control tool
+subprocess.Popen([REMOTE_CONTROL_EXE], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+# Create a capture object for the default camera
+cap = cv2.VideoCapture(1)
+
+prev_time = time.time()
 
 while True:
-    # Read a frame from the video capture device
+    # Capture a frame from the camera
     ret, frame = cap.read()
 
-    # Perform object detection on the frame using the YOLOv5 model
+    # Perform object detection on the image
     results = model(frame)
-
-    # Filter detections based on confidence level
     filtered_detections = [detection for detection in results.xyxy[0] if detection[4] >= confidence_threshold]
 
-    # Draw bounding boxes around the detected objects
     for detection in filtered_detections:
         x1, y1, x2, y2, conf, cls = detection
-        cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 0), 2)
 
-            # Draw the confidence score next to the bounding box
+        # Ignore detections with class IDs for dresses and skirts
+        if cls == skirt_id or cls == short_sleeved_dress_id or cls == long_sleeved_dress_id or cls == vest_dress_id or cls == sling_dress_id:
+            continue
+
+        cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 0), 2)
         label = f'{conf:.2f}'
         text_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 2)[0]
         cv2.rectangle(frame, (int(x1), int(y1 - text_size[1] - 4)), (int(x1 + text_size[0]), int(y1)), (255, 0, 0), -1)
         cv2.putText(frame, label, (int(x1), int(y1 - 2)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
-
         center_x = (x1 + x2) / 2
         center_y = (y1 + y2) / 2
-
-        # Get the center point of the screen
         screen_width, screen_height = frame.shape[1], frame.shape[0]
         screen_center_x, screen_center_y = screen_width / 2, screen_height / 2
 
-        # Calculate the distance between the bounding box center and the screen center
-        distance_to_center = ((center_x - screen_center_x) ** 2 + (center_y - screen_center_y) ** 2) ** 0.5
+        # Check if the object is in the center of the screen
+        if (x1 >= 0 and y1 >= 0 and x2 <= screen_width and y2 <= screen_height) and \
+   (abs(center_x - screen_center_x) < screen_width * 0.1 and abs(center_y - screen_center_y) < screen_height * 0.1):
+            curr_time = time.time()
+            if curr_time - prev_time >= 2:
+                # Capture an image with the remote camera control tool
+                remote_window = pyautogui.getWindowsWithTitle('Remote')[0]
+                remote_window.activate()
 
-        # If the distance is small enough, run the function
-        if distance_to_center < 50:
-            run_gphoto2()
+                # Press the '1' key to capture the image
+                pydirectinput.press('1')
+                print("Remote camera control tool command executed!")
+                prev_time = curr_time
 
-
-
-
-    # Display the frame with the bounding boxes
-
+    # Display the captured image with object detection
     cv2.imshow('Object Detection', frame)
 
-    # Exit if the 'q' key is pressed
     if cv2.waitKey(1) == ord('q'):
         break
 
-# Release the video capture device and close the window
+# Release the capture object and destroy all windows
 cap.release()
 cv2.destroyAllWindows()
